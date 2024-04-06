ee1624f0bbbcda88dfabaa41067750173f4a1aaa
dan00n1 <danine.hbo@gmail.com>
2024-04-06 21:40:46 +0200

    Update chessboard_corner_detection with comments

--- aruco_marker_depthai/scripts/camera_calibration_tests/chessboard_corner_detection.py
+++ aruco_marker_depthai/scripts/camera_calibration_tests/chessboard_corner_detection.py
@@ -1,13 +1,12 @@
 import cv2
-import numpy as np
 import os
 
-# File path to the input image
-FOLDER_PATH = "/home/danoon/shared/aruco-marker-detection/Media/test_images_phone_camera"
-FILE_NAME = "chessboard_input3.jpeg"
+# File path to the input image; change to the correct directory path
+FOLDER_PATH = "/home/danoon/shared/aruco-marker-detection/Media/images_oakd_camera"
+FILE_NAME = "chessboard_oakd_1.jpg" # Change to the correct file name
 FILE_PATH = os.path.join(FOLDER_PATH, FILE_NAME)
 
-NEW_FOLDER_NAME = "drawn_corners"
+NEW_FOLDER_NAME = "drawn_corners" # The name of the folder where the output image will be saved
 
 # Chessboard dimensions
 NUMBER_OF_SQUARES_X = 10 # Nr of chessboard squares along the x-axis
@@ -16,6 +15,17 @@ nX = NUMBER_OF_SQUARES_X - 1 # Nr of interior corners along x-axis
 nY = NUMBER_OF_SQUARES_Y - 1 # Nr of interior corners along y-axis
 
 def prepare_output_directory_and_filename(file_path, folder_name):
+  """ 
+  A function to prepare the output directory and filename for the new image
+  
+  Args:
+    file_path: The path to the input image file
+    folder_name: The name of the folder where the output image will be saved
+    
+  Returns:
+    output_directory: The output directory
+    file_name_without_extension: The file name without the extension
+  """
   # Extract the directory path and file name
   directory, file_name = os.path.split(file_path)
   # Create the output file name by removing the file extension part
@@ -28,18 +38,37 @@ def prepare_output_directory_and_filename(file_path, folder_name):
   return output_directory, file_name_without_extension
 
 def load_image(file_path):
-    """ A function to load an image from the file path """
-    try:
-        # Load an image
-        image = cv2.imread(file_path)
-        if image is None:
-            raise FileNotFoundError("Could not load image")
-        return image
-    except Exception as e:
-        print(f"Error loading image: {e}")
-        return None
+  """ 
+  A function to load an image from the file path
+  If the image could not be loaded, None is returned
+
+  Args:
+    file_path: The path to the image file
+
+  Returns:
+  image: The loaded image 
+  """
+  try:
+      # Load an image
+      image = cv2.imread(file_path)
+      if image is None:
+          raise FileNotFoundError("Could not load image")
+      return image
+  except Exception as e:
+      print(f"Error loading image: {e}")
+      return None
 
 def detect_corners_on_chessboard(image):
+  """
+  A function to detect the corners on the chessboard in the image
+
+  Args:
+    image: The input image
+
+  Returns:
+    corners_found: A boolean indicating if the corners were found
+    corners: The detected corners
+  """
   gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  
  
   # Find the corners on the chessboard
@@ -48,6 +77,16 @@ def detect_corners_on_chessboard(image):
   return corners_found, corners
 
 def draw_corners_on_chessboard_and_save_as_image(image, corners, output_directory, file_name_without_extension):
+  """
+  A function to draw the corners on the chessboard and save the image
+
+  Args:
+    image: The input image
+    corners: The detected corners
+    output_directory: The output directory
+    file_name_without_extension: The file name without the extension
+  """
+
   cv2.drawChessboardCorners(image, (nY, nX), corners, True)
 
   # Create the output file path
@@ -57,7 +96,7 @@ def draw_corners_on_chessboard_and_save_as_image(image, corners, output_director
   cv2.imwrite(new_filename, image)
 
 def display_image(image):
-  """ A function to display the image until any key is pressed"""
+  """ A function to display the image until any key is pressed """
   cv2.imshow("Image", image) 
   cv2.waitKey(0) 
     
@@ -65,6 +104,12 @@ def display_image(image):
   cv2.destroyAllWindows() 
 
 def process_image(file_path):
+  """ 
+  A function to process the image and detect the corners on the chessboard
+  
+  Args:
+    file_path: The path to the image file
+  """
   output_directory, file_name_without_extension = prepare_output_directory_and_filename(file_path, NEW_FOLDER_NAME)    
   image = load_image(file_path)
 
