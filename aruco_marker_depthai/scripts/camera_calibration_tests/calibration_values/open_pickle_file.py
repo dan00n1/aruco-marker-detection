9d47803a6fa276fba52ac27b648bf7ec0dd2ae4f
dan00n1 <danine.hbo@gmail.com>
2024-04-08 16:26:14 +0200

    Add pickle to yaml file option

--- aruco_marker_depthai/scripts/camera_calibration_tests/calibration_values/open_pickle_file.py
+++ aruco_marker_depthai/scripts/camera_calibration_tests/calibration_values/open_pickle_file.py
@@ -1,5 +1,14 @@
+import os
 import pickle
-directory = '/home/danoon/shared/aruco-marker-detection/aruco_marker_depthai/scripts/camera_calibration_tests/calibration_values/camera_calibration_pickle_file.pkl'
-pickle_file = open(directory, 'rb')
+
+# Define the directory and file name
+DIRECTORY = '/home/danoon/shared/aruco-marker-detection/aruco_marker_depthai/scripts/camera_calibration_tests/calibration_values/'
+PICKLE_FILE_NAME = 'camera_calibration_pickle_file.pkl'
+
+pickle_file_path = os.path.join(DIRECTORY, PICKLE_FILE_NAME)
+
+pickle_file = open(pickle_file_path, 'rb')
+
 calibration_values = pickle.load(pickle_file)
+
 print(calibration_values)
---
+++ aruco_marker_depthai/scripts/camera_calibration_tests/calibration_values/open_yaml_file.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+
+# Define the directory and file name
+DIRECTORY = '/home/danoon/shared/aruco-marker-detection/aruco_marker_depthai/scripts/camera_calibration_tests/calibration_values/'
+CV_FILE_NAME = 'calibration_chessboard.yaml'
+
+cv_file_path = os.path.join(DIRECTORY, CV_FILE_NAME)
+
+# Load the parameters from the saved file
+cv_file = cv2.FileStorage(cv_file_path, cv2.FILE_STORAGE_READ) 
+
+mtx = cv_file.getNode('MTX').mat()
+dist = cv_file.getNode('DIST').mat()
+
+cv_file.release()
+
+# Display key parameter outputs of the camera calibration process
+print("Camera matrix:") 
+print(mtx) 
+
+print("\n Distortion coefficient:") 
+print(dist) 
