813fca517f5e99d1912532f7956c77a35406ecdc
dan00n1 <danine.hbo@gmail.com>
2024-04-23 13:21:06 +0200

    Add marker orientation tester

---
+++ aruco_marker_depthai/launch/test_marker_orientation.launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+import launch_ros.actions
+import launch_ros.descriptions
+
+def generate_launch_description():
+    # Has its own pipeline to start the oak-d camera, so no stereo node is needed
+    # Use 'q' to quit the program
+    marker_orientation_tester = launch_ros.actions.Node(package='aruco_marker_depthai', executable='marker_orientation_tester.py', output='screen')
+    
+    ld = LaunchDescription()
+    ld.add_action(marker_orientation_tester)
+
+    return ld
